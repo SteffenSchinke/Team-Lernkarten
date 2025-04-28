af684e5df8ea97c2c61196678ff83c488ab7bb72
Dominic Dobbrunz <d.dobbrunz@gmx.de>
2025-04-28 14:34:56 +0200

    Setting bearbeitet

--- app/src/main/java/de/syntax_institut/lernkarten/team/ui/components/GeneralSettings.kt
+++ app/src/main/java/de/syntax_institut/lernkarten/team/ui/components/GeneralSettings.kt
@@ -5,16 +5,19 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import de.syntax_institut.lernkarten.team.ui.viewmodels.SettingsVM
 
 @Composable
 fun GeneralSettings() {
-    val darkModeEnabled = remember { mutableStateOf(false) }
+    val isDarkTehem by SettingsVM.isDarkTheme.collectAsState()
     val notificationsEnabled = remember { mutableStateOf(true) }
 
     Column {
@@ -36,8 +39,8 @@ fun GeneralSettings() {
             name = "Nachtmodus",
             action = {
                 Switch(
-                    checked = darkModeEnabled.value,
-                    onCheckedChange = { darkModeEnabled.value = it },
+                    checked = isDarkTehem,
+                    onCheckedChange = SettingsVM::toggleThemeMode,
                     modifier = Modifier.padding(start = 8.dp)
                 )
             }
--- app/src/main/java/de/syntax_institut/lernkarten/team/ui/components/ProfileSection.kt
+++ app/src/main/java/de/syntax_institut/lernkarten/team/ui/components/ProfileSection.kt
@@ -1,16 +1,17 @@
 package de.syntax_institut.lernkarten.team.ui.components
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -21,17 +22,17 @@ fun ProfileSection() {
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier.fillMaxWidth()
     ) {
-        Image(
-            painter = painterResource(id = android.R.drawable.sym_def_app_icon),
-            contentDescription = "Profilbild",
-            modifier = Modifier.size(100.dp)
-        )
+     Icon(Icons.Default.Person,"Person",
+         Modifier.size(32.dp))
+
+
+
 
         Spacer(modifier = Modifier.height(8.dp))
 
-        Text("Max Mustermann", fontSize = 20.sp)
+        Text(text = "Max Mustermann", fontSize = 20.sp)
         Spacer(modifier = Modifier.height(4.dp))
-        Text("@maxmustermann", fontSize = 16.sp)
+        Text(text = "@maxmustermann", fontSize = 16.sp)
     }
 }
 
